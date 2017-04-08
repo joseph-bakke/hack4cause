775aed5d9d4c4b26249b8f5af8d9be6b68a57a6b
Megan McMillan <mmcmillan@cbtnuggets.com>
2017-04-08 15:29:15 -0700

    in progress

--- node/server/routes/api.js
+++ node/server/routes/api.js
@@ -2,6 +2,7 @@ const agent = require('superagent');
 const moment = require('moment');
 const _ = require('lodash');
 const util = require('util');
+const Promise = require('bluebird');
 
 module.exports = function (app) {
     const connections = app.get('connections');
@@ -11,6 +12,27 @@ module.exports = function (app) {
 
     // eslint-disable-next-line
     const geoLib = require('../lib/geocode')(app);
+
+    function parseLocation(location) {
+        console.log(`Parsing Location ${location}`);
+        const parsed = location.split(' ');
+        console.log(parsed.length);
+        _.forEach(parsed, function (name) {
+            console.log(name);
+            // if name is special character, drop
+            // if name is ST, AVE drop
+            // if name equals W/S
+
+            // save first instance that passes as street
+            // save second instance that passes as crossStreet
+        });
+
+        const street = `${parsed[0]} ${parsed[1]}`;
+        const crossStreet = `${parsed[2]} ${parsed[3]}`;
+
+        // eslint-disable-next-line
+        return {street: street, crossStreet: crossStreet};
+    }
 
     // const config = app.get('config');
 
@@ -80,11 +102,91 @@ module.exports = function (app) {
         });      
     });
 
-    app.get('/connect', function (req, res) {
-        console.log('Sending sample api request to google geocoding api');
-        geoLib.convertAddressToLatLong('2700 CENTENNIAL BLVD, Eugene OR')
-            .then(response => res.status(200).send(response))
-            .catch(err => res.status(500).send(err));
+    app.get('/parking', function (req, res) {
+        const dbname = 'parkingCit2007';
+
+        db.all(`SELECT lat from ${dbname} LIMIT 1`, function (err, response) {
+            if (err) {
+                console.log('Adding lat column to db');
+                db.run(`ALTER TABLE ${dbname} ADD COLUMN lat TEXT`);
+            } else {
+                console.log(response);
+                console.log('lat column exists');
+            }
+        });
+
+        db.all(`SELECT lng from ${dbname} LIMIT 1`, function (err, response) {
+            if (err) {
+                console.log('Adding lng column to db');
+                db.run(`ALTER TABLE ${dbname} ADD COLUMN lng TEXT`);
+            } else {
+                console.log(response);
+                console.log('lng column exists');
+            }
+        });
+
+        db.all(`SELECT street from ${dbname} LIMIT 1`, function (err, response) {
+            if (err) {
+                console.log('Adding street column to db');
+                db.run(`ALTER TABLE ${dbname} ADD COLUMN street TEXT`);
+            } else {
+                console.log(response);
+                console.log('street column exists');
+            }
+        });
+
+        db.all(`SELECT crossStreet from ${dbname} LIMIT 1`, function (err, response) {
+            if (err) {
+                console.log('Adding crossStreet column to db');
+                db.run(`ALTER TABLE ${dbname} ADD COLUMN crossStreet TEXT`);
+            } else {
+                console.log(response);
+                console.log('crossStreet column exists');
+            }
+        });
+
+        // iterate through db and convert addresses to lat long
+        db.all(`SELECT  * FROM ${dbname}`, function (err, record) {
+            if (err) {
+                console.log(`error: ${err}`);
+            }
+            let count = 0;
+            let updated = 0;
+            // db.run(`UPDATE ${dbname} SET lat=null`);
+            // db.run(`UPDATE ${dbname} SET lng=null`);
+            record.forEach(function (row) {
+                count++;
+                // if lat long values are null call google api to convert
+                console.log(`Converting location: ${row.Location}`);
+                const loc = parseLocation(row.Location);
+                console.log(loc);
+
+                console.log(`UPDATE ${dbname} SET street="${loc.street}" WHERE Location="${row.Location}"`);
+                console.log(`UPDATE ${dbname} SET crossStreet="${loc.crossStreet}" WHERE Location="${row.Location}"`);
+                // db.run(`UPDATE ${dbname} SET street="${loc.street}" WHERE Location="${row.Location}"`);
+                // db.run(`UPDATE ${dbname} SET crossStreet="${loc.crossStreet}" WHERE Location="${row.Location}"`);
+                updated++;
+
+                // if (row.lat == null || row.lng == null) {
+                //     geoLib.convertAddressToLatLong(`${loc.street} & ${loc.crossStreet}, Eugene OR`)
+                //         .then(response => {
+                //             console.log('Udpating db with values');
+                //             console.log(`UPDATE ${dbname} SET lat=${response.lat} WHERE Location="${row.Location}"`);
+                //             console.log(`UPDATE ${dbname} SET lng=${response.lng} WHERE Location="${row.Location}"`);
+                //             db.run(`UPDATE ${dbname} SET lat=${response.lat} WHERE Location="${row.Location}"`);
+                //             db.run(`UPDATE ${dbname} SET lng=${response.lng} WHERE Location="${row.Location}"`);
+
+                //             updated++;
+                //         })
+                //         .catch(error => console.log(`ERROR OCCURRED AT API.JS: ${error}`));
+                // } else {
+                //     console.log('Skipping crossStreet, already has lat and lng');
+                //     console.log(row.crossStreet);
+                // }
+            });
+            // res.status(200).send('reset to null');
+            res.status(200).send(`Found ${count} records, updated ${updated}`);
+        });      
     });
 
     /**
@@ -151,6 +253,5 @@ module.exports = function (app) {
                 console.log(err);
                 res.status(500).send(err);
             });
-
     });
 };
