dd8d1227c3b517ecef972ee638c19726d9e37ab3
Joe Bakke <jbakke@cbtnuggets.com>
2017-04-09 09:10:07 -0700

    clean up

--- node/server/routes/index.js
+++ node/server/routes/index.js
@@ -1,35 +1,3 @@
-const _ = require('lodash');
-const fs = require('fs');
-
-// TODO get rid of this stuff
-const getRoutes = function (path) {
-    const routes = [];
-    const traverseFileSystem = function (currentPath) {
-        const files = fs.readdirSync(currentPath);
-        // eslint-disable-next-line
-        for (const i in files) {
-            // eslint-disable-next-line
-            if (files.hasOwnProperty(i)) {
-                const currentFile = `${currentPath}/${files[i]}`;
-                const stats = fs.statSync(currentFile);
-                if (stats.isFile() && (/\.(js)$/i).test(currentFile) && currentFile !== __filename) {
-                    routes.push(currentFile);
-                } else if (stats.isDirectory() && files[i] !== 'utils') {
-                    traverseFileSystem(currentFile);
-                }
-            }
-        }
-    };
-    traverseFileSystem(path);
-    return routes;
-};
-
-const routes = getRoutes(__dirname);
-
 module.exports = function (app) {
-    // Hook up the routes contained in the subdirs within routes
-    _.forEach(routes, function (route) {
-        // eslint-disable-next-line
-        require(route)(app);
-    });
+    require('./api')(app);
 };
